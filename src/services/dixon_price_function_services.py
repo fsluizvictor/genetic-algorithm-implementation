0e7b06914059b7fadc45cd55626d3344b8d0e48d
fsluizvictor <fs.luizvictor@gmail.com>
2021-07-10 19:15:58 -0300

    solving problems with dixon price function

--- src/services/dixon_price_function_services.py
+++ src/services/dixon_price_function_services.py
@@ -6,22 +6,20 @@ from src.services.individual_services import IndividualServices
 class DixonPriceFunctionService(IndividualServices):
 
     def to_rate(self, individual: Individual) -> float:
+        print(individual.genes)
         individual.genes = list(map(lambda i: 2 ** (-((2 ** i) - 2) / 2 ** i), individual.genes))
         print(individual.genes)
         gene_1 = individual.genes[0]
         term_1 = (gene_1 - 1) ** 2
         sum = 0
+
         for i in range(1, len(individual.genes)):
             gene = individual.genes[i]
             older_gene = individual.genes[i - 1]
             new = i * (2 * (gene ** 2) - older_gene) ** 2
             sum += new
 
-        return term_1 + sum
-
-    dimension = len(self.chromosome)
-    result = 0
-
+        return int(term_1 + sum)
 
 
 def main():
@@ -30,7 +28,7 @@ def main():
     ind.genes = [1, 2]
     print(ind.genes)
     teste = DixonPriceFunctionService()
-    print(teste.to_rate(ind))
+    print(int(teste.to_rate(ind)))
 
 
 if __name__ == "__main__":
